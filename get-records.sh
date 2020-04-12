f330ee77128496d2e1a13358dfb7bdff41b6c262
VJean <jean@vintache.eu>
2020-04-12 16:55:39 +0200

    change in dataset fields

--- get-records.sh
+++ get-records.sh
@@ -17,10 +17,10 @@ curl -o $DL_FILE -s 'https://opendata.paris.fr/api/records/1.0/search/?dataset=v
 test -f "${MD5_FILE}" && md5sum --status -c "${MD5_FILE}" && echo_log "nothing new" && exit 0
 
 # let's build a csv file with the following headers :
-# timestamp, station_name, lat, lon, standard_bikes, e_bikes, nb_docks, free_docks 
+# timestamp, station_name, lat, lon, mechanical, ebike, capacity, numdocksavailable, numbikesavailable
 TIMESTAMP=$(cat $DL_FILE | jq '.records[0].record_timestamp')
 NB_RECORDS=$(cat $DL_FILE | jq '.records | length')
-cat $DL_FILE | jq -r '.records[] as $r | [$r.record_timestamp,$r.fields.station_name,$r.geometry.coordinates[0],$r.geometry.coordinates[1],$r.fields.nbbike,$r.fields.nbebike,$r.fields.nbedock,$r.fields.nbfreeedock] | @csv' >> ${DL_DIR}/$(date +"%Y%m%d")-velib-records.csv
+cat $DL_FILE | jq -r '.records[] as $r | [$r.record_timestamp,$r.fields.name,$r.geometry.coordinates[0],$r.geometry.coordinates[1],$r.fields.mechanical,$r.fields.ebike,$r.fields.capacity,$r.fields.numdocksavailable,$r.fields.numbikesavailable] | @csv' >> ${DL_DIR}/$(date +"%Y%m%d")-velib-records.csv
 echo_log "$NB_RECORDS records with timestamp ${TIMESTAMP}"
 
 # compute checksum for the next pass
